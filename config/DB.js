b017fcb9d3e9f5e557bec2f59d5c853449ccc3f7
amitdas1880 <amitdas1880@gmail.com>
2024-06-07 00:33:22 +0530

    changes

--- config/DB.js
+++ config/DB.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://amitdas1880:9934180962@cluster0.glxqprh.mongodb.net/food-del')
+    await mongoose.connect(process.env.DATABASE)
     .then(() => console.log("---***MongoDB connected successfully***---"));
 }
